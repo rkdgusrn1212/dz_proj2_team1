3cf0d2bbd4a6a47a23d692a61ed02c207c9edce6
rkdgusrn1212 <khgkjg12@naver.com>
2022-11-13 20:27:45 +0900

    필요없는 import 제거

--- src/main/java/team1/mini2/dz3/auth/core/AuthConfig.java
+++ src/main/java/team1/mini2/dz3/auth/core/AuthConfig.java
@@ -1,24 +1,14 @@
 package team1.mini2.dz3.auth.core;
 
-import java.io.IOException;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.logout.LogoutHandler;
-import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 
